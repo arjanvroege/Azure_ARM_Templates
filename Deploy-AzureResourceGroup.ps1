5eefd47b0a54a196bed67107536ffdfbb04bf1ef
Arjan Vroege <arjanvroege@hotmail.com>
2016-12-05 21:54:25 +0100

    Add files via upload

---
+++ Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,26 @@
+﻿Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory=$true)] $TemplateFile,
+    [string] [Parameter(Mandatory=$true)] $ParamatersFile,
+    [boolean] [Parameter(Mandatory=$true)] $Test,
+	[boolean] [Parameter(Mandatory=$true)] $Customer
+)
+
+Login-AzureRmAccount
+
+# Create or update the resource group using the specified template file and template parameters file
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop 
+
+if($test -eq $true) {
+    Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -TemplateParameterFile $ParamatersFile `
+                                   -Verbose
+} else {
+    New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                   -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -TemplateParameterFile $ParamatersFile `
+                                   -Force -Verbose
+}
